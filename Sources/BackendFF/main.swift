9d43d092a695847ce0539b20d57b21ec1b0f4be7
Daniel Tull <dt@danieltull.co.uk>
2023-03-20 11:39:34 +0000

    Add an example of using the Backend library

--- Sources/BackendFF/main.swift
+++ Sources/BackendFF/main.swift
@@ -3,10 +3,30 @@ import Backend
 import FlyingFox
 import Foundation
 
+struct Users: Rule {
+    let id: Int
+    var rules: some Rule {
+        "User \(id)"
+    }
+}
+
+struct Home: Rule {
+    var rules: some Rule {
+
+        Users(id: 1)
+            .path("users")
+
+        "Home"
+    }
+}
+
 let server = HTTPServer(port: 8002) { request in
-    let path = request.path
-    let body = Data(path.utf8)
-    return HTTPResponse(statusCode: .ok, body: body)
+
+    guard let response = Home().run(environment: EnvironmentValues(request: Request(path: request.path))) else {
+        return HTTPResponse(statusCode: .notFound)
+    }
+
+    return HTTPResponse(statusCode: HTTPStatusCode(response.statusCode, phrase: ""), body: response.body)
 }
 
 try await server.start()
